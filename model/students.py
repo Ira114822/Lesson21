b423862fdd222b142cefa84f32540947fe84ee8b
Ira114822 <odaa1976.com@gmail.com>
2022-11-15 21:50:30 +0300

    55

--- model/students.py
+++ model/students.py
@@ -1,16 +1,24 @@
 from human import Human
-
+from marknote import MarkNote
 
 class Student(Human):
-    def __init__(self, name, age, sex, mark, alive):
+    def __init__(self, name, age, sex, marknote, alive):
         self.name = name
         self.age = age
         self.sex = sex
-        self.mark = mark
+        if isinstance(marknote, MarkNote):
+            self.note = marknote
+        else:
+            self.note = MarkNote()
+
+        self.note.add(mark)
         self.alive = alive
 
     def study(self):
         pass
+
+    def add(self, mark):
+        self.note.add(mark)
 
     def __str__(self):
         return f"{self.name}: age = {self.age};" \
